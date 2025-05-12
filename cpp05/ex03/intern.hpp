7ec0978e404d604316d351041d3b0701e9273634
chhoflac <chhoflac@student.42.fr>
2025-05-12 12:41:34 +0200

    few corrections

--- cpp05/ex03/intern.hpp
+++ cpp05/ex03/intern.hpp
@@ -6,12 +6,13 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 18:20:41 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/12 10:51:57 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/12 12:38:27 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERN_HPP
 # define INTERN_HPP
+
 # include <string>
 # include "AForm.hpp"
 # include "PresidentialPardonForm.hpp"
