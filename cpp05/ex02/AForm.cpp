297f0009b223dd97c9f577a4f0f38aa80ea797f6
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-14 20:27:04 +0200

    adding corrections

--- cpp05/ex02/AForm.cpp
+++ cpp05/ex02/AForm.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 09:56:47 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/14 20:14:24 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/14 20:26:35 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,13 +94,12 @@ void AForm::beSigned(Bureaucrat &signer){
 	}
 }
 
-void AForm::execute(const Bureaucrat &executor) const{
+void AForm::execute(const Bureaucrat &executor){
 	if (!this->getIsSigned()){
 		throw FormNotSignedException();
 	}
 	if (this->getExecuteGrade() < executor.getGrade()){
 		throw GradeTooLowException();
 	}
-	
-	
+	this->doExecution();
 }
