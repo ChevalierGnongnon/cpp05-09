c0f15949bc770ee5006de172c4c2cacd9207d0fc
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-16 14:31:04 +0200

    adding exceptions

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,12 +6,28 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:29:40 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/15 15:46:47 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/16 14:06:25 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-void ScalarConverter::convert(const std::string &represent){
-	
+void ScalarConverter::convert(const std::string &input){
+	if (input == "+inff" || input == "nanf" || input == "-inff"){
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << input << std::endl;
+        std::cout << "double: " << input << std::endl;
+        return ;
+    }
+    else if (input == "nan" || input == "+inf" || input == "-inf"){
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << input << "f" << std::endl;
+        std::cout << "double: " << input << std::endl;
+        return;
+    }
+    else {
+        
+    }
 }
