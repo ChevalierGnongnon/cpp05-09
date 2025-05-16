6fffb33de977d34ee71de8a918934c478c129ec4
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-16 17:48:59 +0200

    add files

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,13 +6,16 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:29:40 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/16 15:48:35 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/16 17:31:31 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 void ScalarConverter::convert(const std::string &input){
+    bool number = true;
+    bool negate = false;
+    
 	if (input == "+inff" || input == "nanf" || input == "-inff"){
         std::cout << "char: impossible" << std::endl;
         std::cout << "int: impossible" << std::endl;
@@ -35,6 +38,25 @@ void ScalarConverter::convert(const std::string &input){
             std::cout << "double: " << static_cast<double>(input[0]) << std::endl;
             return;
         }
-        
+        if (input[0] == '-')
+            negate = true;
+        if ((input[0] == '-' && input.length() > 1) || isdigit(input[0]))
+        {
+            int i = 0;
+            if (input[0] == '-')
+                i++;
+            while (i < input.length()){
+                if (!isdigit(input[i])){
+                    number = false;
+                }
+                i++;
+            }
+            if (!number){
+                
+            }
+            else {
+                
+            }
+        }
     }
 }
