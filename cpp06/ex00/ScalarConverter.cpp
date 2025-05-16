b628dd73fff3bef133b09281230eec691063c7b8
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-16 17:02:58 +0200

    adding exceptions

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:29:40 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/16 14:06:25 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:48:35 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,13 @@ void ScalarConverter::convert(const std::string &input){
         return;
     }
     else {
+        if (input.length() == 1 && isprint(input[0])){
+            std::cout << "char: " << input << std::endl;
+            std::cout << "int: " << static_cast<int>(input[0])<< std::endl;
+            std::cout << "float: " << static_cast<float>(input[0]) << "f" << std::endl;
+            std::cout << "double: " << static_cast<double>(input[0]) << std::endl;
+            return;
+        }
         
     }
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:01:56 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/15 15:43:17 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:37:43 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SCALARCONVERTER_HPP
 # include <string>
 # include <iostream>
+# include <cctype>
 
 class ScalarConverter{
 	private :
