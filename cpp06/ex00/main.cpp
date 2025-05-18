de7a7827543f3e0f146b40c2ab9069c15ce7ffef
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-18 14:17:16 +0200

    ex00 done

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,12 +6,46 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:36:37 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/15 15:36:57 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/18 14:01:19 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 int main(void){
-	
+	ScalarConverter::convert("a");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("42");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("4.2f");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("4.2");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("nan");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("notanumber");
+     std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert(".42");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("42..");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("-4.2");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("nanf");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("inff");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("inf");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("++42");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("--42");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("127");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("128");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("-1");
+    std::cout << "---------------------" << std::endl;
+    ScalarConverter::convert("0");
 }
