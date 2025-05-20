72c4aa2e76c38749257509bb8af91c5299c7f2ab
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-05-20 17:06:37 +0200

    files

--- cpp06/ex02/Identify.cpp
+++ cpp06/ex02/Identify.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:35:17 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/05/20 16:47:28 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/05/20 17:05:20 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <iostream>
 
 Base    *generate(void){
     int res = rand() % 3;
@@ -36,10 +37,42 @@ Base    *generate(void){
     return (ret);
 }
 
-void    identify(Base *p){
-    
+void identify(Base* p) {
+    if (dynamic_cast<A*>(p) != NULL) {
+        std::cout << "A" << std::endl;
+    }
+    else if (dynamic_cast<B*>(p) != NULL) {
+        std::cout << "B" << std::endl;
+    }
+    else if (dynamic_cast<C*>(p) != NULL) {
+        std::cout << "C" << std::endl;
+    }
+    else {
+        std::cout << "Unknown Type" << std::endl;
+    }
 }
 
 void    identify(Base &p){
+    try {
+        A& a = dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return ;
+    } catch (std::bad_cast&) {
+        
+    }
+    try {
+        B& b = dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return ;
+    } catch (std::bad_cast&) {
+        
+    }
+    try {
+        C& c = dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return ;
+    } catch (std::bad_cast&) {
+
+    }
     
 }
